e8b1b4ba4a2a3eda9be9702f91cc04e8052f4d23
Ben Jokela <benjokela@gmail.com>
2020-02-16 09:42:09 -0400

    3.2 and 3.3 complete

--- NavDemo/app/src/main/java/com/example/navdemo/MainFragment.kt
+++ NavDemo/app/src/main/java/com/example/navdemo/MainFragment.kt
@@ -2,23 +2,68 @@ package com.example.navdemo
 
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.widget.Button
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.example.navdemo.databinding.FragmentMainBinding
 
 /**
  * A simple [Fragment] subclass.
  */
 class MainFragment : Fragment() {
 
+
+    private lateinit var binding: FragmentMainBinding
+    private lateinit var navController: NavController
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_main,
+            container,
+            false
+        )
+        setHasOptionsMenu(true)
+        return binding.root
     }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.overflow_menu, menu)
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item, navController) || super.onOptionsItemSelected(item)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?){
+        super.onViewCreated(view, savedInstanceState)
+        navController = view.findNavController()
+
+        binding.apply{
+            sendMoneyBtn.setOnClickListener {
+                navController.navigate(MainFragmentDirections.actionMainFragmentToChooseRecipientFragment())
+
+            }
+            viewTransactionsBtn.setOnClickListener {
+                navController.navigate(MainFragmentDirections.actionMainFragmentToViewTransactionFragment())
+            }
+            viewBalanceBtn.setOnClickListener {
+                navController.navigate(MainFragmentDirections.actionMainFragmentToViewBalanceFragment2())
+            }
+        }
+    }
+    //override fun onClick...
 
 
 }
