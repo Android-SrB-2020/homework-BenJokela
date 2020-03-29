3effd35abcd583702e1aa5993942611120f2e276
Ben Jokela <benjokela@gmail.com>
2020-03-28 21:26:20 -0300

    7.4 complete

--- 7Recycler/RecyclerViewClickHandler-Starter/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
+++ 7Recycler/RecyclerViewClickHandler-Starter/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
@@ -220,6 +220,18 @@ class SleepTrackerViewModel(
         }
     }
 
+    private val _navigateToSleepDetail = MutableLiveData<Long>()
+    val navigateToSleepDetail
+        get() = _navigateToSleepDetail
+
+    fun onSleepNightClicked(id: Long) {
+        _navigateToSleepDetail.value = id
+    }
+
+    fun onSleepDetailNavigated() {
+        _navigateToSleepDetail.value = null
+    }
+
     /**
      * Executes when the CLEAR button is clicked.
      */
